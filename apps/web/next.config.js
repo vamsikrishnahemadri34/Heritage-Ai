02c1531a7fc2762abeccf39332bb5d54a009f3e1
Hemadri Nagasai Vamsi Krishna <vamsikrishnahemadri34@gmail.com>
2026-08-24 11:43:50 +0530

    fix: allow Cloudinary production images

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
         port: "8000",
         pathname: "/media/**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
     ],
   },
 };
